ebb2f2007b66070ef8eb406c9954533f0e4975a1
Aidar Maratbekov <maratbekovaidar@gmail.com>
2021-11-19 01:47:27 +0600

    Spring Initializr project

---
+++ src/main/java/kz/maratbekovaidar/forum/AituStudentsForumRestServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.maratbekovaidar.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AituStudentsForumRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AituStudentsForumRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/maratbekovaidar/forum/AituStudentsForumRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.maratbekovaidar.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AituStudentsForumRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
